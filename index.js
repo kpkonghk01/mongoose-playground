001b1cbdd503afa28a8bc151f8123dac857abe6b
Kong Ki Pan <kpkong@hk01.com>
2023-08-28 17:53:21 +0800

    feat: test

--- index.js
+++ index.js
@@ -10,68 +10,42 @@ const agent = require('./db/setup');
 const { Schema, model } = mongoose;
 
 (async () => {
-  const PenNameGroupSchema = new Schema(
+  const TestSchema = new Schema(
     {
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
+      name: {
+        type: String,
         required: true,
       },
-    },
-    { _id: false, timestamps: { createdAt: true, updatedAt: false } }
-  );
-
-  const PenNameSchema = new Schema(
-    {
-      penNameGroups: {
-        type: [PenNameGroupSchema],
-        default: [],
+      meta: {
+        type: mongoose.Schema.Types.Mixed,
       },
     },
-    { timestamps: true }
+    { timestamps: true, minimize: false }
   );
 
-  const PenName = model('PenName', PenNameSchema);
+  const Test = model('Test', TestSchema);
 
-  const penNameGroups = [
-    {
-      _id: new mongoose.Types.ObjectId(),
-    },
-    {
-      _id: new mongoose.Types.ObjectId(),
-    },
-    {
-      _id: new mongoose.Types.ObjectId(),
-    },
-  ];
-
-  const [penName] = await Promise.all([
-    PenName.create({
-      penNameGroups,
+  const [test] = await Promise.all([
+    Test.create({
+      name: 'test',
+      meta: {
+        foo: 'a',
+        bar: {},
+      },
     }),
   ]);
 
-  let results = await PenName.find({}).lean().exec();
+  let results = await Test.find({}).lean().exec();
   console.log('Created:', JSON.stringify(results, null, 2));
-
-  const newPenNameGroups = [
-    {
-      _id: new mongoose.Types.ObjectId(),
-    },
-    {
-      _id: new mongoose.Types.ObjectId(),
-    },
-  ];
-  const newPenNameGroupIds = newPenNameGroups.map((_) => _._id.toString());
 
   await new Promise((_) => setTimeout(_, 1000));
 
-  await PenName.findByIdAndUpdate(
-    penName._id,
+  await Test.findByIdAndUpdate(
+    test._id,
     {
-      $push: {
-        penNameGroups: {
-          $each: newPenNameGroupIds.map((_id) => ({ _id })),
-        },
+      meta: {
+        foo: 'b',
+        bar: {},
       },
     },
     {
@@ -79,27 +53,8 @@ const { Schema, model } = mongoose;
     }
   );
 
-  results = await PenName.find({}).lean().exec();
+  results = await Test.find({}).lean().exec();
   console.log('Updated:', JSON.stringify(results, null, 2));
-
-  const deletedPenNameGroupIds = [
-    penNameGroups[0],
-    penNameGroups[2],
-    newPenNameGroups[1],
-  ].map((_) => _._id.toString());
-
-  await PenName.findByIdAndUpdate(penName._id, {
-    $pull: {
-      penNameGroups: {
-        _id: {
-          $in: deletedPenNameGroupIds,
-        },
-      },
-    },
-  });
-
-  results = await PenName.find({}).lean().exec();
-  console.log('Deleted:', JSON.stringify(results, null, 2));
 
   return results;
 })().then(
